b88f6e5907232e136e3cdc44e96647e84a0eac4f
Jeff Yaeger <jeffisreallyneat@gmail.com>
2023-05-05 20:50:47 -0700

    Moved Entity Setter interface and empty implementation into this project. Added SetDefaults method to EntityTransaction.cs

--- EntityTransaction.cs
+++ EntityTransaction.cs
@@ -1,10 +1,27 @@
 namespace EntityDefaults
 {
+    using System;
+    using EntityModels;
+
     public class EntityTransaction
     {
         public object Entity { get; set; }
         public TransactionState State { get; set; }
+
+        public void SetDefaults<TKey>(TKey id, params object[] objects) where TKey : IEquatable<TKey>
+        {
+            switch (State)
+            {
+                case TransactionState.Add when Entity is IOnAdd<TKey> addEntity:
+                    addEntity.OnAdd(id, objects);
+                    break;
+                case TransactionState.Update when Entity is IOnUpdate<TKey> updateEntity:
+                    updateEntity.OnUpdate(id, objects);
+                    break;
+                case TransactionState.Delete when Entity is IOnDelete<TKey> deleteEntity:
+                    deleteEntity.OnDelete(id, objects);
+                    break;
+            }
+        }
     }
 }
-
-
